7dc35b0a3c9ebca31a9e741333249a58b76c0d81
Stephen Harris <trackmastersteve@users.noreply.github.com>
2018-10-13 06:41:36 -0700

    Add files via upload

---
+++ pynmap.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+#
+# pynmap.py
+#
+
+import optparse
+import nmap
+
+def nmapScan(tgtHost, tgtPort):
+    nmScan = nmap.PortScanner()
+    nmScan.scan(tgtHost, tgtPort)
+    state = nmScan[tgtHost]['tcp'][int(tgtPort)]['state']
+    if state == 'open':
+            st = '[+]'
+    else:
+        st = '[-]'
+    print(st + " " + tgtHost + " tcp/" +tgtPort + " -" + state)
+
+def main():
+    parser = optparse.OptionParser(('usage %prog -H <target host> -p <target port(s) separated by comma>'))
+    parser.add_option("-H", dest="tgtHost", type="string", help="specify target host")
+    parser.add_option("-p", dest="tgtPort", type="string", help="specify target port(s) separated by comma")
+    (options, args) = parser.parse_args()
+    tgtHost = str(options.tgtHost).strip()
+    tgtPorts = [s.strip() for s in str(options.tgtPort).split(',')]
+    if (tgtHost == None) | (tgtPorts[0] == None):
+        print(parser.usage)
+        exit(0)
+    for tgtPort in tgtPorts:
+        nmapScan(tgtHost, tgtPort.strip())
+
+main()
